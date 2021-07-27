0efadb5e89b8bb3296a2b317b034f490fbb9f9e3
karan51ngh <karansingh9535@gmail.com>
2021-07-27 12:11:05 +0530

    input template

---
+++ bashCheatSheets/2.input.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/bash
+# if we enter the command "./script_input.sh Tyrell Elliot Alderson"
+# $1 becomes Tyrell $2 becomes Elliot $3 becomes Alderson
+echo $1 $2 $3
+# $0 is the script name
+echo ""
+echo $0
