d998bfb79699b37edcd293e79e3b08df8c75e212
karan51ngh <karansingh9535@gmail.com>
2021-07-27 12:57:56 +0530

    CLI

--- bashCheatSheets/13.arrays.sh
+++ bashCheatSheets/13.arrays.sh
@@ -1,1 +1,19 @@
+#! /usr/bin/bash
+car=('BMW' 'TOYOTA' 'HONDA' 'SUZUKI')
+echo ${car[@]} #@ prints entire array
+# printing by index
+unset car[2] #to remove a particular value
+car[2]='MERCEDES'
+echo ""
+for i in {0..2}; do
+	echo ${car[$i]}
+done
+echo ${!car[@]} #prints the indices
 
+echo ""
+for i in ${!car[@]}; do
+	echo ${car[$i]}
+done
+echo ${!car[@]} #prints the indices
+echo ""
+echo ${#car[@]} #prints the number of indices
