db7caba0aa4d12381a97507f326c258ceb17b09c
karan51ngh <karansingh9535@gmail.com>
2021-07-27 12:49:24 +0530

    pipes

---
+++ bashCheatSheets/16.pipes.sh
@@ -0,0 +1,7 @@
+# !/usr/bin/bash
+# used to send output from 1 script to another
+MESSAGE="Hello Friend!"
+# touch /home/karan51ngh/Programming/Bash/temp/second_script.sh
+# chmod +x /home/karan51ngh/Programming/Bash/temp/second_script.sh
+export MESSAGE
+bash /home/karan51ngh/Programming/Bash/temp/second_script.sh
